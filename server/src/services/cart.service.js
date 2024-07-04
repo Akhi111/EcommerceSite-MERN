e305d3dfb74940ab9084298e33010d1ff85435d7
Akhi111 <acharekarakhil@gmail.com>
2024-07-04 21:29:44 +0530

    added cart service

--- server/src/services/cart.service.js
+++ server/src/services/cart.service.js
@@ -1,4 +1,6 @@
 import Cart from "../models/cart.model.js";
+import CartItem from "../models/cartItem.model.js";
+import Product from "../models/product.model.js";
 
 async function createCart(user) {
   try {
@@ -6,7 +8,82 @@ async function createCart(user) {
     const createdCart = await cart.save();
     return createdCart;
   } catch (error) {
-    throw new Error(error.message);
+    throw new Error(`Error creating cart: ${error.message}`);
   }
 }
-export default createCart;
+
+async function findUserCart(userId) {
+  try {
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      throw new Error("Cart not found");
+    }
+
+    let cartItems = await CartItem.find({ cart: cart._id }).populate("product");
+    cart.cartItems = cartItems;
+
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+
+    for (let cartItem of cart.cartItems) {
+      totalPrice += cartItem.price * cartItem.quantity;
+      totalDiscountedPrice += cartItem.discountedPrice * cartItem.quantity;
+      totalItem += cartItem.quantity;
+    }
+
+    cart.totalPrice = totalPrice;
+    cart.totalItem = totalItem;
+    cart.discount = totalPrice - totalDiscountedPrice;
+
+    return cart;
+  } catch (error) {
+    throw new Error(`Error finding user cart: ${error.message}`);
+  }
+}
+
+async function addCartItem(userId, req) {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      throw new Error("Cart not found");
+    }
+
+    const product = await Product.findById(req.productId);
+    if (!product) {
+      throw new Error("Product not found");
+    }
+
+    const existingCartItem = await CartItem.findOne({
+      cart: cart._id,
+      product: product._id,
+      userId,
+    });
+
+    if (existingCartItem) {
+      // Increase quantity if item already exists in the cart
+      existingCartItem.quantity += 1;
+      await existingCartItem.save();
+      return "Item quantity updated in cart";
+    } else {
+      //Create new cart item if not present
+      const cartItem = new cartItem({
+        product: product._id,
+        cart: cart._id,
+        quantity: 1,
+        userId,
+        price: product.price,
+        size: req.size,
+        discountedPrice: product.discountedPrice,
+      });
+
+      const createdCartItem = await cartItem.save();
+      cart.cartItems.push(createdCartItem);
+      await cart.save();
+      return "Item added to cart";
+    }
+  } catch (error) {
+    throw new Error(`Error adding item to cart: ${error.message}`);
+  }
+}
+export default { createCart, findUserCart, addCartItem };
