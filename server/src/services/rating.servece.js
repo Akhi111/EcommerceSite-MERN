374180a5534ad456c61831c0811101571eadb18d
Akhi111 <acharekarakhil@gmail.com>
2024-07-05 20:02:31 +0530

    added review & rating services

---
+++ server/src/services/rating.servece.js
@@ -0,0 +1,21 @@
+import Rating from "../models/rating.model.js";
+import findProductById from "../services/product.service.js";
+
+async function createRating(req, user) {
+  const product = await findProductById(req.productId);
+
+  const rating = new Rating({
+    product: product._id,
+    user: user._id,
+    reating: req.rating,
+    createdAt: new Date(),
+  });
+
+  return await rating.save();
+}
+
+async function getProductRating(productId) {
+  return await Rating.find({ product: productId });
+}
+
+export default { createRating, getProductRating };
